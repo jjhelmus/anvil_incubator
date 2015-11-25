1dab15ab00402a6606dda046b763c130234c82ad
Jonathan Helmus <jjhelmus@gmail.com>
2015-11-25 13:19:51 -0600

    Debugging

--- run_docker_build.sh
+++ run_docker_build.sh
@@ -22,6 +22,9 @@ conda install --yes conda-build jinja2 anaconda-client
 conda info
 ls
 pwd
+ls recipe
+which python
+which conda
 
 # build packages
 conda build -q -c jjhelmus --python 2.7 --numpy 1.10 recipe
