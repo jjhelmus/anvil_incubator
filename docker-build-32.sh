d70ceb613ccbcbc2b62406045cd46938d3f312cb
Jonathan Helmus <jjhelmus@gmail.com>
2015-11-29 18:28:06 -0600

    Add 32 bit builds

---
+++ docker-build-32.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Activate Holy Build Box environment.
+source /hbb_exe/activate
+
+# install Miniconda
+set -x
+curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86.sh
+bash Miniconda3-latest-Linux-x86.sh -b -p /anaconda
+PATH=/opt/rh/devtoolset-2/root/usr/bin:/opt/rh/autotools-latest/root/usr/bin:/anaconda/bin:$PATH
+
+# update conda, install build tools
+conda update -yq conda
+conda install -yq conda-build jinja2 anaconda-client
+
+# debugging
+conda info
+ls
+pwd
+which python
+which conda
+
+# build packages
+conda build -q -c jjhelmus --python 2.7 --numpy 1.10 /io/recipe
+conda build -q -c jjhelmus --python 3.3 --numpy 1.9 /io/recipe
+conda build -q -c jjhelmus --python 3.4 --numpy 1.10 /io/recipe
+conda build -q -c jjhelmus --python 3.5 --numpy 1.10 /io/recipe
+
+# upload packages
+cp /anaconda/conda-bld/*/*.tar.bz2 .
+ls *.tar.bz2
+#anaconda -t $TOKEN upload *.tar.bz2
