213d9a9dae1ab37330249480a29c8e2dbcaebb79
Jonathan Helmus <jjhelmus@gmail.com>
2016-02-10 18:53:25 -0600

    Disable Circle CI, Enable Travis CI

---
+++ travis_build.sh
@@ -0,0 +1,8 @@
+# build script of Mac OS X on Travis CI
+
+if [ ! -f build_script_osx-64.sh ]; then
+    echo "Creating build script from YAML file"
+    python create_build_script.py build_specs.yml osx-64 > build_script_linux-64.sh
+    chmod +x build_script_osx-64.sh
+fi
+./build_script_osx-64.sh
