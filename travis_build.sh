b237a5cabcd4a70a9f8b5b9b099fe91b93cc6fb3
Jonathan Helmus <jjhelmus@gmail.com>
2016-02-10 19:15:50 -0600

    Fix script name

--- travis_build.sh
+++ travis_build.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f build_script_osx-64.sh ]; then
     echo "Creating build script from YAML file"
-    python create_build_script.py build_specs.yml osx-64 > build_script_linux-64.sh
+    python create_build_script.py build_specs.yml osx-64 > build_script_osx-64.sh
     chmod +x build_script_osx-64.sh
 fi
 ./build_script_osx-64.sh
