7d94194b3ee3a1fb5b213479c576906b6eb32439
Jonathan Helmus <jjhelmus@gmail.com>
2016-02-10 19:54:47 -0600

    AppVeyor build script

---
+++ build_script_win-32.bat
@@ -0,0 +1,11 @@
+SET PYTHON_VERSION=2.7.10
+'%CMD_IN_ENV% conda build -q --python 2.7 1.10 recipe' 
+
+SET PYTHON_VERSION=3.3.x
+'%CMD_IN_ENV% conda build -q --python 3.3 recipe' 
+
+SET PYTHON_VERSION=3.4.x
+'%CMD_IN_ENV% conda build -q --python 3.4 recipe' 
+
+SET PYTHON_VERSION=3.5.x
+'%CMD_IN_ENV% conda build -q --python 3.5 recipe' 
