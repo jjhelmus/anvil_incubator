41bb1bf457dbb723900cc90c74dcd01a1d2c3d5e
Jonathan Helmus <jjhelmus@gmail.com>
2016-02-10 21:06:10 -0600

    AppVeyor build from spec file

--- create_build_script.py
+++ create_build_script.py
@@ -8,11 +8,18 @@ import yaml
 
 def create_single_command(options):
     command = 'conda build -q '
+
     if 'python' in options:
         command += '--python %s ' % (options['python'])
     if 'numpy' in options:
         command += '--numpy %s ' % (options['numpy'])
     command += options['recipe_dir']
+
+    if options['platform'].startswith('win'):
+        command = '%CMD_IN_ENV% ' + command
+        if 'python' in options:
+            prefix = 'SET PYTHON_VERSION=%s.x\r\n' % (options['python'])
+            command = prefix + command
     return command
 
 
@@ -34,5 +41,7 @@ if __name__ == "__main__":
     yaml_file = sys.argv[1]
     target_platform = sys.argv[2]
 
+    if target_platform.startswith('win'):
+        print('ECHO ON')
     for data in  yaml.load_all(open(yaml_file, 'r')):
         print_commands_for_target(data, target_platform)
