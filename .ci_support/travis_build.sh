302a456e11aa79eb791154ca82896759a9b80162
Jonathan Helmus <jjhelmus@gmail.com>
2016-02-11 20:02:14 -0600

    Single file .travis.yml

--- .ci_support/travis_build.sh
+++
@@ -1,8 +0,0 @@
-# build script of Mac OS X on Travis CI
-
-if [ ! -f build_script_osx-64.sh ]; then
-    echo "Creating build script from YAML file"
-    python create_build_script.py build_specs.yml osx-64 > build_script_osx-64.sh
-    chmod +x build_script_osx-64.sh
-fi
-./build_script_osx-64.sh
